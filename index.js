ac4ad185d5b5227a81afebea6f7588a28fb22d6c
Ditul-Roy <ditulr@gmail.com>
2023-04-28 08:10:29 +0600

    the dragon news server

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port =process.env.PORT || 5000;
+
+const catagories = require('./data/catagoris.json')
+const news = require('./data/news.json')
+
+app.use(cors())
+
+app.get('/', (req, res) =>{
+    res.send('Dragon is running')
+});
+
+app.get('/news', (req, res) =>{
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedId = news.find(n => n._id === id );
+    res.send(selectedId) 
+})
+
+app.get('/catagories/:id', (req, res) =>{
+    const id = parseInt(req.params.id);
+    if(id === 0){
+        res.send(news)
+    }
+    else{
+        const catagoriesId = news.filter(n => parseInt(n.category_id) === id);
+        res.send(catagoriesId);   
+    }
+})
+
+app.get('/catagories', (req, res) =>{
+    res.send(catagories)
+})
+
+app.listen(port, () =>{
+    console.log(`Dragon api is running on port: ${port}`);
+});
